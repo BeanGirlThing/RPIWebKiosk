76bee38adbf6ea0df7f94e46fa17bbbcbf0cf09c
Merhlim <joshua.ampstead@hotmail.co.uk>
2018-08-22 11:08:29 +0100

    Working on fixing geckodriver

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ echo "Setting up and installing dependancies (Auto run has to be configured manu
 sudo apt-get install python3-pip
 sudo apt-get install firefox-esr
 export PATH=$PATH:./files/geckodriver-v0.21.0-linux32.tar.gz
+sudo chmod 775 ./files/geckodriver-v0.21.0-linux32.tar.gz
 #Firefox is the core browser used for the kiosk
 sudo chmod 775 ./*
 #Make sure every file has the 775 permission
