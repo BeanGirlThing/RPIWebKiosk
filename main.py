17188567b1f2149bb3118e0f612ae9ce1678a3d0
Jessica <jessica@JessicasGayDebian.GayDebian>
2018-09-10 21:47:25 +0100

    chrome works, just minor tweaks and features to go

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import configparser # Importing dependancies to allow the program to work
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import os
+from time import sleep
+import socket
+import httplib
 
 class main: # Defining the main object
 	config = configparser.ConfigParser() # Create a configuration object 
@@ -13,17 +16,29 @@ class main: # Defining the main object
 	kiosk = config.get("config", "kiosk") # Check wether the browser will run in kiosk mode
 	options = Options() # Create a chrome options object
 	if kiosk == "True": # If the browser is supposed to run in kiosk mode
-                options.add_arguement("--kiosk") # Set an arguement to make the browser run in kiosk mode
+                options.add_argument("--kiosk") # Set an arguement to make the browser run in kiosk mode
 
 	def __init__(self): # Initialisation
-		print("Booting up webbrowser with current config!") # Info 
-		browser = webdriver.Chrome(executable_path=r"./chromedriver",chrome_options=self.options()) # Create the browser object using the profile created previously
-		self.browser.get(self.webpage) # Setting the browser to the website set in config
-																
+		print("Booting up webbrowser with current config, on page '"+self.webpage+"'!") # Info 
+		browser = webdriver.Chrome(chrome_options=self.options) # Create the browser object using the profile created previously
+		browser.get(self.webpage)
+		while self.browserstatus(browser):
+			print(str(self.browserstatus(browser)))
+			sleep(0.1)
 
+	def browserstatus(self,browser):
+		try:
+			browser.execute(Command.STATUS)
+			return True
+		except(socket.error, httplib.CannotSendRequest):
+			return False
+		except:
+			raise socket.error
 if __name__ == "__main__": # If the script is ran
 	main() # run the main class 
 	config = configparser.ConfigParser() # Create a configuration object
 	config.read("./config.ini") # Read the config file
+	print("Terminating program")
 	if config.read("config","shutdown_on_close") == "True": # If shutdown on close is true
 		os.system("sudo halt") # Shutdown the system
+
