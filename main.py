b49384d9c259e2609746866dbfcbe17ddd0e9c87
Jessica <29659463+Merhlim@users.noreply.github.com>
2018-09-10 11:17:40 +0100

    Initial code commit for the chrome testing branch

--- main.py
+++ main.py
@@ -1,34 +1,29 @@
-#RPIWebKiosk - Written by Joshua Ampstead
+#RPIWebKiosk - Written by Joshua Ampstead - merhlim.me
 #Please read the licence for further information
 
 import configparser # Importing dependancies to allow the program to work
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import os
 
 class main: # Defining the main object
 	config = configparser.ConfigParser() # Create a configuration object 
 	config.read("./config.ini") # Assigning the "config.ini" file to the configuration object
-	plugins = config.get("config","firefox_addons") # Getting information from the config file to find the name of the firefox extentions
-	plugins = plugins.split(";") # Spliting up the list of XPI files (Firefox extentions)
 	webpage = config.get("config", "webpage") # Getting the webpage address from the config
-	ffprofile = webdriver.FirefoxProfile() # Creating a firefox profile object
-
+	kiosk = config.get("config", "kiosk") # Check wether the browser will run in kiosk mode
+        options = Options() # Create a chrome options object
+	if kiosk == "True": # If the browser is supposed to run in kiosk mode
+                options.add_arguement("--kiosk") # Set an arguement to make the browser run in kiosk mode
 
 	def __init__(self): # Initialisation
 		print("Booting up webbrowser with current config!") # Info 
-		self.add_ext() # Function that adds extentions onto the firefox profile
-		browser = webdriver.Firefox(firefox_profile=self.ffprofile) # Create the browser object using the profile created previously
+		browser = webdriver.Chrome(executable_path=r"./chromedriver",chrome_options=self.options()) # Create the browser object using the profile created previously
 		self.browser.get(self.webpage) # Setting the browser to the website set in config
-
-
-	def add_ext(self): # Function to add extentions onto the firefox profile
-		for i in range(0, len(self.plugins)): # Repeat for the number of firefox addons written in the config
-			self.ffprofile.add_extension(extension="./files/firefox_addons/" + self.plugins[i]) # Add an extention to the firefox profilee 
 																
 
 if __name__ == "__main__": # If the script is ran
 	main() # run the main class 
 	config = configparser.ConfigParser() # Create a configuration object
 	config.read("./config.ini") # Read the config file
-	if config.read("config","shutdown_on_close") == "true": # If shutdown on close is true
+	if config.read("config","shutdown_on_close") == "True": # If shutdown on close is true
 		os.system("sudo halt") # Shutdown the system
